4690db2606ff0105bc2503361ba1cdd4f3320b8b
mamadmrm <mamadmrm2019@gmail.com>
2026-05-21 22:41:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
+import os
+import flask
+from threading import Thread
+
+# ترفند گول زدن رندر برای پورت
+app = flask.Flask('')
+@app.route('/')
+def home(): return "Bot is running!"
+def run(): app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
+Thread(target=run).start()
+
+# ----------------------------------------------------
+# بقیه کدهای قبلی رباتت از اینجا به بعد باشد...
+
 import telebot
 from telebot import types
 import requests
