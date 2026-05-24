45444f5021611487b619f42d41b0a5d5949b4c83
mamadmrm <mamadmrm2019@gmail.com>
2026-05-24 16:37:18 +0330

    Update database.js

--- database.js
+++ database.js
@@ -1,24 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const DB_FILE = path.join(__dirname, 'database.json');
+const DB_FILE = './database.json';
 
 function loadDB() {
-  try {
-    const data = fs.readFileSync(DB_FILE, 'utf8');
-    return JSON.parse(data);
-  } catch (e) {
-    return { users: {}, payments: {}, configs: {} };
+  if (!fs.existsSync(DB_FILE)) {
+    const initial = { users: {}, configs: {} };
+    fs.writeFileSync(DB_FILE, JSON.stringify(initial, null, 2));
+    return initial;
   }
+  return JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
 }
 
 function saveDB(data) {
   fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
-}
-
-function getUser(userId) {
-  const db = loadDB();
-  return db.users[userId] || null;
 }
 
 function createUser(userId, username, firstName) {
@@ -26,28 +21,49 @@ function createUser(userId, username, firstName) {
   if (!db.users[userId]) {
     db.users[userId] = {
       id: userId,
-      username: username || '',
-      firstName: firstName || '',
-      createdAt: new Date().toISOString(),
+      username: username,
+      firstName: firstName,
+      purchases: [],
       hasFreeTest: false,
-      purchases: []
+      createdAt: new Date().toISOString()
     };
     saveDB(db);
   }
   return db.users[userId];
 }
 
+function getUser(userId) {
+  const db = loadDB();
+  return db.users[userId];
+}
+
+function getAllUsers() {
+  const db = loadDB();
+  return Object.values(db.users);
+}
+
 function addPurchase(userId, planId, config, expireDate) {
   const db = loadDB();
   if (db.users[userId]) {
     db.users[userId].purchases.push({
-      planId,
-      config,
-      purchaseDate: new Date().toISOString(),
-      expireDate
+      planId: planId,
+      config: config,
+      expireDate: expireDate,
+      purchaseDate: new Date().toISOString()
     });
     saveDB(db);
   }
+}
+
+function updateConfig(planId, config) {
+  const db = loadDB();
+  db.configs[planId] = config;
+  saveDB(db);
+}
+
+function getConfig(planId) {
+  const db = loadDB();
+  return db.configs[planId];
 }
 
 function setFreeTestUsed(userId) {
@@ -58,30 +74,12 @@ function setFreeTestUsed(userId) {
   }
 }
 
-function getAllUsers() {
-  const db = loadDB();
-  return Object.values(db.users);
-}
-
-function updateConfig(key, value) {
-  const db = loadDB();
-  db.configs[key] = value;
-  saveDB(db);
-}
-
-function getConfig(key) {
-  const db = loadDB();
-  return db.configs[key] || null;
-}
-
 module.exports = {
-  loadDB,
-  saveDB,
-  getUser,
   createUser,
-  addPurchase,
-  setFreeTestUsed,
+  getUser,
   getAllUsers,
+  addPurchase,
   updateConfig,
-  getConfig
+  getConfig,
+  setFreeTestUsed
 };
