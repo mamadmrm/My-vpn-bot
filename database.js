198234e4b4f6541c1eeeeb803cd34ee1373ffce7
mamadmrm <mamadmrm2019@gmail.com>
2026-05-24 19:05:41 +0330

    Update database.js

--- database.js
+++ database.js
@@ -1,85 +1,141 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
 
-const DB_FILE = './database.json';
+const DB_FILE = "database.json";
 
-function loadDB() {
-  if (!fs.existsSync(DB_FILE)) {
-    const initial = { users: {}, configs: {} };
-    fs.writeFileSync(DB_FILE, JSON.stringify(initial, null, 2));
-    return initial;
-  }
-  return JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
+if (!fs.existsSync(DB_FILE)) {
+ fs.writeFileSync(DB_FILE, JSON.stringify({
+  users: [],
+  configs: [],
+  purchases: [],
+  pendingPayments: []
+ }, null, 2));
 }
 
-function saveDB(data) {
-  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+function read() {
+ return JSON.parse(fs.readFileSync(DB_FILE));
 }
 
-function createUser(userId, username, firstName) {
-  const db = loadDB();
-  if (!db.users[userId]) {
-    db.users[userId] = {
-      id: userId,
-      username: username,
-      firstName: firstName,
-      purchases: [],
-      hasFreeTest: false,
-      createdAt: new Date().toISOString()
-    };
-    saveDB(db);
-  }
-  return db.users[userId];
-}
-
-function getUser(userId) {
-  const db = loadDB();
-  return db.users[userId];
-}
-
-function getAllUsers() {
-  const db = loadDB();
-  return Object.values(db.users);
-}
-
-function addPurchase(userId, planId, config, expireDate) {
-  const db = loadDB();
-  if (db.users[userId]) {
-    db.users[userId].purchases.push({
-      planId: planId,
-      config: config,
-      expireDate: expireDate,
-      purchaseDate: new Date().toISOString()
-    });
-    saveDB(db);
-  }
-}
-
-function updateConfig(planId, config) {
-  const db = loadDB();
-  db.configs[planId] = config;
-  saveDB(db);
-}
-
-function getConfig(planId) {
-  const db = loadDB();
-  return db.configs[planId];
-}
-
-function setFreeTestUsed(userId) {
-  const db = loadDB();
-  if (db.users[userId]) {
-    db.users[userId].hasFreeTest = true;
-    saveDB(db);
-  }
+function write(data) {
+ fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
-  createUser,
-  getUser,
-  getAllUsers,
-  addPurchase,
-  updateConfig,
-  getConfig,
-  setFreeTestUsed
+
+ createUser(id, username, firstName) {
+  const db = read();
+
+  const exists = db.users.find(u => u.id == id);
+
+  if (!exists) {
+   db.users.push({
+    id,
+    username,
+    firstName,
+    hasFreeTest: false
+   });
+
+   write(db);
+  }
+ },
+
+ getUser(id) {
+  const db = read();
+  return db.users.find(u => u.id == id);
+ },
+
+ setFreeTestUsed(id) {
+  const db = read();
+
+  const user = db.users.find(u => u.id == id);
+
+  if (user) {
+   user.hasFreeTest = true;
+   write(db);
+  }
+ },
+
+ addConfig(type, config) {
+  const db = read();
+
+  db.configs.push({
+   id: Date.now() + Math.random(),
+   type,
+   config,
+   used: false
+  });
+
+  write(db);
+ },
+
+ getUnusedConfig(type) {
+  const db = read();
+
+  return db.configs.find(c =>
+   c.type === type && !c.used
+  );
+ },
+
+ useConfig(id) {
+  const db = read();
+
+  const cfg = db.configs.find(c => c.id == id);
+
+  if (cfg) {
+   cfg.used = true;
+   write(db);
+  }
+ },
+
+ addPurchase(userId, type, config) {
+  const db = read();
+
+  db.purchases.push({
+   userId,
+   type,
+   config
+  });
+
+  write(db);
+ },
+
+ getPurchases(userId) {
+  const db = read();
+
+  return db.purchases.filter(p =>
+   p.userId == userId
+  );
+ },
+
+ setPendingPayment(userId, plan) {
+  const db = read();
+
+  db.pendingPayments = db.pendingPayments.filter(
+   p => p.userId != userId
+  );
+
+  db.pendingPayments.push({
+   userId,
+   plan
+  });
+
+  write(db);
+ },
+
+ getPendingPayment(userId) {
+  const db = read();
+
+  return db.pendingPayments.find(
+   p => p.userId == userId
+  );
+ },
+
+ clearPendingPayment(userId) {
+  const db = read();
+
+  db.pendingPayments = db.pendingPayments.filter(
+   p => p.userId != userId
+  );
+
+  write(db);
+ }
 };
